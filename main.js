3992886b97bf965bbe17a98927b5edf377bcc140
NahomDoesProgramming <84427393+NahomDoesProgramming@users.noreply.github.com>
2022-02-12 17:27:29 -0800

    Update main.js

--- main.js
+++ main.js
@@ -28,3 +28,64 @@ function speak()
     var utterThis = new SpeechSynthesisUtterance(speach_data1 + speach_data2);
     synth.speak(utterThis)
 }
+function check()
+{
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+function gotResult()
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML = results[0].label;
+        document.getElementById("result_gesture_name2").innerHTML = results[1].label;
+        prediction1 = results[0].label;
+        prediction2 = results[1].label;
+        speak();
+        if(results[0].label == "Pointing")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128072; &#128073;";
+        }
+        if(results[0].label == "OK")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+        if(results[0].label == "Crossed Fingers")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#129310;";
+        }
+        if(results[0].label == "Thumbs Up")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if(results[0].label == "Thumbs Down")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128078;";
+        }
+        if(results[1].label == "Pointing")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128072; &#128073;";
+        }
+        if(results[1].label == "OK")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+        if(results[1].label == "Crossed Fingers")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#129310;";
+        }
+        if(results[1].label == "Thumbs Up")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if(results[1].label == "Thumbs Down")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128078;";
+        }
+    }
+}
